ed3588804983bb60cd4167297efae3d919b2eeb9
Bri Dotson <bridotson@gmail.com>
2014-11-06 09:35:06 -0800

    remove warnings from .rspec

--- brigid2718/brews/db/schema.rb
+++ brigid2718/brews/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105231139) do
+ActiveRecord::Schema.define(version: 20141106172632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20141105231139) do
     t.string   "date_brewed"
     t.float    "batch_size"
     t.string   "beer_style"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
