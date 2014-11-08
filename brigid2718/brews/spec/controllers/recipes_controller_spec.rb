bbeda184c5db4f1ce1f4390c0c5b7632a9db0089
Bri Dotson <bridotson@gmail.com>
2014-11-08 12:09:38 -0800

    fix test for index response for recipe controller

--- brigid2718/brews/spec/controllers/recipes_controller_spec.rb
+++ brigid2718/brews/spec/controllers/recipes_controller_spec.rb
@@ -5,4 +5,9 @@ describe RecipesController do
     expect(RecipesController.new).to respond_to :index
   end
 
+  it "should get index" do
+    get 'index'
+    expect(response).to render_template(:index)
+  end
+
 end
